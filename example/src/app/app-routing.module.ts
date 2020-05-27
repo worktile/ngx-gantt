fd0cb81ff0103dcfde5fff6912a6e211ff8c22ac
why520crazy <why520crazy@163.com>
2020-05-27 15:16:24 +0800

    docs: beautify examples and docs use docgeni

--- example/src/app/app-routing.module.ts
+++ example/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppExamplesComponent } from './examples/examples.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'examples',
+        component: AppExamplesComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ example/src/app/examples/examples.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-examples-gantt',
+    templateUrl: './examples.component.html',
+    styleUrls: ['./examples.component.scss'],
+})
+export class AppExamplesComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
