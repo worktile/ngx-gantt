8cd242049b7bc5ee1e6860812ab3868af9cba906
walkekay <walkerkay@163.com>
2020-05-28 14:30:09 +0800

    chore: merge

--- packages/gantt/src/calendar/calendar.component.ts
+++ packages/gantt/src/calendar/calendar.component.ts
@@ -1,27 +1,18 @@
-import {
-    Component,
-    OnInit,
-    HostBinding,
-    OnChanges,
-    SimpleChanges,
-    ChangeDetectorRef,
-    OnDestroy,
-    NgZone,
-    Inject,
-} from '@angular/core';
+import { Component, OnInit, HostBinding, OnChanges, SimpleChanges, ChangeDetectorRef, OnDestroy, NgZone, Inject } from '@angular/core';
 import { GanttDatePoint } from '../class/date-point';
 import { Subject } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { GanttRef, GANTT_REF_TOKEN } from '../gantt-ref';
+import { headerHeight } from '../gantt.styles';
 
 @Component({
     selector: 'gantt-calendar-overlay',
-    templateUrl: './calendar.component.html',
+    templateUrl: './calendar.component.html'
 })
 export class GanttCalendarComponent implements OnInit, OnChanges, OnDestroy {
     public height = 500;
 
-    public headerHeight = 60;
+    public headerHeight = headerHeight;
 
     public todayPoint: {
         x: number;
@@ -39,11 +30,7 @@ export class GanttCalendarComponent implements OnInit, OnChanges, OnDestroy {
 
     @HostBinding('class.gantt-calendar-overlay') className = true;
 
-    constructor(
-        @Inject(GANTT_REF_TOKEN) private ganttRef: GanttRef,
-        private cdr: ChangeDetectorRef,
-        private ngZone: NgZone
-    ) {}
+    constructor(@Inject(GANTT_REF_TOKEN) private ganttRef: GanttRef, private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
 
     ngOnInit() {
         this.ngZone.onStable.pipe(take(1)).subscribe(() => {
