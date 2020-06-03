39a9efb65362641d1f31f55338d8b0e9342766bb
zhangwen <1062680993@qq.com>
2020-06-03 10:37:19 +0800

    fix(gantt-table): add ngx-gantt-table component

--- packages/gantt/src/gantt.module.ts
+++ packages/gantt/src/gantt.module.ts
@@ -9,16 +9,18 @@ import { GanttComponent } from './gantt.component';
 import { GanttMainComponent } from './main/gantt-main.component';
 import { GanttIconComponent } from './icon/icon.component';
 import { GanttDragBackdropComponent } from './drag-backdrop/drag-backdrop.component';
+import { GanttTableMainComponent } from './table/main/table-mail.component';
 
 @NgModule({
     imports: [CommonModule],
-    exports: [GanttComponent, GanttFlatComponent, GanttTableColumnComponent],
+    exports: [GanttComponent, GanttFlatComponent, GanttTableMainComponent, GanttTableColumnComponent],
     declarations: [
         GanttComponent,
         GanttFlatComponent,
         GanttTableColumnComponent,
         GanttMainComponent,
         GanttTableComponent,
+        GanttTableMainComponent,
         GanttCalendarComponent,
         GanttBarComponent,
         GanttIconComponent,
