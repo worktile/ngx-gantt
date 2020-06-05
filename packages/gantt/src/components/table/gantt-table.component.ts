a031c2ccc5e0c462447ac6cdeba347bdfe14bc42
zhangwen <1062680993@qq.com>
2020-06-05 13:23:28 +0800

    fix(table): change table to flex div

--- packages/gantt/src/components/table/gantt-table.component.ts
+++ packages/gantt/src/components/table/gantt-table.component.ts
@@ -1,6 +1,6 @@
-import { Component, HostBinding, TemplateRef, QueryList, Input, OnInit, Inject } from '@angular/core';
+import { Component, HostBinding, TemplateRef, QueryList, Input, OnInit, Inject, ElementRef } from '@angular/core';
 import { GanttItemInternal, GanttGroupInternal } from '../../class';
-import { maxSideWidth, minSideWidth } from '../../gantt.styles';
+import { maxSideWidth, minSideWidth, sideWidth } from '../../gantt.styles';
 import { GANTT_REF_TOKEN, GanttRef } from '../../gantt-ref';
 import { NgxGanttTableColumnComponent } from '../../table/gantt-column.component';
 
@@ -19,6 +19,10 @@ export class GanttTableComponent implements OnInit {
         return minSideWidth;
     }
 
+    public get sideWidth() {
+        return sideWidth;
+    }
+
     @Input() groups: GanttGroupInternal[];
 
     @Input() items: GanttItemInternal[];
@@ -26,18 +30,44 @@ export class GanttTableComponent implements OnInit {
     @Input()
     set columns(columns: QueryList<NgxGanttTableColumnComponent>) {
         this.columnList = columns;
+        this.resetColumnWidth();
     }
 
     @Input() groupTemplate: TemplateRef<any>;
 
     @HostBinding('class.gantt-table') ganttTableClass = true;
 
-    constructor(@Inject(GANTT_REF_TOKEN) public ganttRef: GanttRef) {}
+    constructor(@Inject(GANTT_REF_TOKEN) public ganttRef: GanttRef, private elementRef: ElementRef) {}
 
     ngOnInit() {}
 
     expandGroup(group: GanttGroupInternal) {
         group.expand = !group.expand;
         this.ganttRef.detectChanges();
+    }
+
+    resetColumnWidth() {
+        const columnsLength = this.columnList.length;
+        this.columnList.forEach((column, index) => {
+            if (columnsLength === 1) {
+                if (index === 0) {
+                    column.width = '100%';
+                }
+            }
+            if (columnsLength === 2) {
+                if (index === 0) {
+                    column.width = '65%';
+                } else {
+                    column.width = '35%';
+                }
+            }
+            if (columnsLength >= 3) {
+                if (index === 0) {
+                    column.width = '45%';
+                } else {
+                    column.width = (0.55 / (columnsLength - 1)) * 100 + '%';
+                }
+            }
+        });
     }
 }
