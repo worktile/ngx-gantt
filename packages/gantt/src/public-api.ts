fc4abc259bebcde8c5a9a9fdffef2c4bf737ae70
walkekay <walkerkay@163.com>
2020-06-09 16:04:55 +0800

    chore: export flat component

--- packages/gantt/src/public-api.ts
+++ packages/gantt/src/public-api.ts
@@ -6,4 +6,5 @@ export * from './gantt.module';
 export * from './gantt.component';
 export * from './table/gantt-table.component';
 export * from './table/gantt-column.component';
+export * from './flat/gantt-flat.component';
 export * from './class';
