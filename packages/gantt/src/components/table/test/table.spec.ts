a57d54666a7092dfb66b9ce358f7c38a9fae1cb4
Guoxiin <guoxiin@163.com>
2021-05-23 17:52:26 +0800

    feat: add gantt table  test #INFR-1716

---
+++ packages/gantt/src/components/table/test/table.spec.ts
@@ -0,0 +1,135 @@
+import { fakeAsync, TestBed, ComponentFixture, async, inject, flush } from '@angular/core/testing';
+import { Component, ViewChild, DebugElement } from '@angular/core';
+import { NgxGanttModule } from 'ngx-gantt';
+import { mockItems, mockGroups } from './mocks';
+import { By } from '@angular/platform-browser';
+import { dispatchMouseEvent } from '@angular/cdk/testing/';
+import { GanttTableComponent } from '../gantt-table.component';
+@Component({
+    selector: 'test-gantt-table',
+    template: `
+        <ngx-gantt #gantt [items]="items" [groups]="groups">
+            <ngx-gantt-table>
+                <ngx-gantt-column [width]="200" name="标题">
+                    <ng-template #cell let-item="item">
+                        {{ item.title }}
+                    </ng-template>
+                </ngx-gantt-column>
+                <ngx-gantt-column>
+                    <ng-template #header> <span style="font-weight: bold;">开始时间</span> </ng-template>
+                    <ng-template #cell let-item="item">
+                        {{ item.start * 1000 | date: 'yyyy-MM-dd' }}
+                    </ng-template>
+                </ngx-gantt-column>
+            </ngx-gantt-table>
+        </ngx-gantt>
+    `,
+    providers: []
+})
+export class TestGanttTableComponent {
+    @ViewChild(GanttTableComponent, { static: true }) ganttTableComponent: GanttTableComponent;
+
+    items = mockItems;
+
+    groups = mockGroups;
+
+    constructor() {}
+}
+
+describe('GanttTable', () => {
+    let component: TestGanttTableComponent;
+    let fixture: ComponentFixture<TestGanttTableComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [NgxGanttModule],
+            declarations: [TestGanttTableComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestGanttTableComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create gantt-table', () => {
+        expect(component).toBeDefined();
+        const ganttTable: DebugElement = fixture.debugElement.query(By.directive(GanttTableComponent));
+        expect(ganttTable).toBeTruthy();
+        expect(ganttTable.nativeElement).toBeTruthy();
+    });
+
+    it('should expand groups', () => {
+        const ganttTable: DebugElement = fixture.debugElement.query(By.directive(GanttTableComponent));
+        expect(ganttTable.query(By.css('.gantt-table-group-title')).nativeNode.classList.contains('expanded')).toBeTruthy();
+        ganttTable.query(By.css('.gantt-table-group-title')).nativeNode.click();
+        fixture.detectChanges();
+        expect(ganttTable.query(By.css('.gantt-table-group-title')).nativeNode.classList.contains('expanded')).toBeFalsy();
+    });
+
+    it('should expand children', () => {
+        const ganttTable: DebugElement = fixture.debugElement.query(By.directive(GanttTableComponent));
+        const tableChildrenLength = ganttTable.query(By.css('.gantt-table-body')).children.length;
+        ganttTable.nativeElement.querySelector('.gantt-table-item-with-group .gantt-expand-icon .expand-icon').click();
+        fixture.detectChanges();
+        expect(ganttTable.query(By.css('.gantt-table-body')).children.length).toEqual(tableChildrenLength + 1);
+    });
+
+    it('should column drag', fakeAsync(() => {
+        const ganttTable: DebugElement = fixture.debugElement.query(By.directive(GanttTableComponent));
+        const dragTrigger = ganttTable.nativeElement.querySelector('.gantt-table-header .gantt-table-column .gantt-table-drag-trigger');
+        fixture.detectChanges();
+        flush();
+
+        const dragTriggerRight = dragTrigger.getBoundingClientRect().right;
+
+        dispatchMouseEvent(dragTrigger, 'mousedown');
+        fixture.detectChanges();
+        flush();
+
+        dispatchMouseEvent(document, 'mousemove', 200);
+        fixture.detectChanges();
+        flush();
+        fixture.detectChanges();
+
+        dispatchMouseEvent(document, 'mousemove', 250);
+        fixture.detectChanges();
+        flush();
+        fixture.detectChanges();
+
+        dispatchMouseEvent(document, 'mouseup');
+        fixture.detectChanges();
+        flush();
+
+        expect(dragTrigger.getBoundingClientRect().right).not.toBe(dragTriggerRight);
+    }));
+
+    it('should table drag', fakeAsync(() => {
+        const ganttTable: DebugElement = fixture.debugElement.query(By.directive(GanttTableComponent));
+        const dragTrigger = ganttTable.nativeElement.children[2];
+        fixture.detectChanges();
+
+        const dragTriggerRight = dragTrigger.getBoundingClientRect().right;
+
+        dispatchMouseEvent(dragTrigger, 'mousedown');
+        fixture.detectChanges();
+        flush();
+
+        dispatchMouseEvent(document, 'mousemove', 250, 150);
+        fixture.detectChanges();
+        flush();
+        fixture.detectChanges();
+
+        dispatchMouseEvent(document, 'mousemove', 50, 50);
+        fixture.detectChanges();
+        flush();
+        fixture.detectChanges();
+
+        dispatchMouseEvent(document, 'mouseup');
+        fixture.detectChanges();
+        flush();
+
+        expect(dragTrigger.getBoundingClientRect().left).not.toBe(dragTriggerRight);
+    }));
+});
