75a6930b7f57235571ef0f4cfa713caa99bca13b
mengshuicmq <mengshuicmq@163.com>
2021-06-18 11:30:35 +0800

    feat: support week view and add test(#INFR-1996)

--- packages/gantt/src/views/factory.ts
+++ packages/gantt/src/views/factory.ts
@@ -4,12 +4,14 @@ import { GanttDate } from '../utils/date';
 import { GanttViewType } from '../class/view-type';
 import { GanttViewQuarter } from './quarter';
 import { GanttViewDay } from './day';
-
+import { GanttViewWeek } from './week';
 
 export function createViewFactory(type: GanttViewType, start: GanttViewDate, end: GanttViewDate, options?: GanttViewOptions) {
     switch (type) {
         case GanttViewType.month:
             return new GanttViewMonth(start, end, options);
+        case GanttViewType.week:
+            return new GanttViewWeek(start, end, options);
         case GanttViewType.quarter:
             return new GanttViewQuarter(start, end, options);
         case GanttViewType.day:
