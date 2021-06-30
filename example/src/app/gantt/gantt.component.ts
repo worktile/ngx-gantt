9566282b49437036bd823d54cd6393991fbb11f5
mengshuicmq <mengshuicmq@163.com>
2021-06-30 09:54:39 +0800

    fix: fix demo

--- example/src/app/gantt/gantt.component.ts
+++ example/src/app/gantt/gantt.component.ts
@@ -28,7 +28,7 @@ export class AppGanttExampleComponent implements OnInit {
     groups = mockGroups;
 
     options = {
-        viewType: GanttViewType.year,
+        viewType: GanttViewType.month,
         draggable: true,
         linkable: true,
         async: true,
