2677d36120d52a8ddb47553bfc4e2abee12c5cd6
zhangwen <1062680993@qq.com>
2021-07-01 23:32:55 +0800

    fix: fix karma conf

--- packages/gantt/karma.conf.js
+++ packages/gantt/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
+            require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
@@ -22,10 +22,13 @@ module.exports = function (config) {
             },
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
+        jasmineHtmlReporter: {
+            suppressAll: true // removes the duplicated traces
+        },
+        coverageReporter: {
             dir: require('path').join(__dirname, '../../coverage/gantt'),
-            reports: ['html', 'lcovonly', 'text-summary'],
-            fixWebpackSourcePaths: true
+            fixWebpackSourcePaths: true,
+            reporters: [{ type: 'html' }, { type: 'lcovonly' }, { type: 'text-summary' }]
         },
         reporters: ['progress', 'kjhtml'],
         port: 9876,
