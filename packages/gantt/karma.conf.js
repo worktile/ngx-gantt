143fe3e067bfe5aaa92b8c757bb043d88d93517d
zhangwen <1062680993@qq.com>
2021-07-08 17:03:16 +0800

    fix: fix coverageReporter dir

--- packages/gantt/karma.conf.js
+++ packages/gantt/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
         //     suppressAll: true // removes the duplicated traces
         // },
         coverageReporter: {
-            dir: require('path').join(__dirname, '../../coverage/gantt'),
+            dir: require('path').join(__dirname, '../../coverage'),
             fixWebpackSourcePaths: true,
             reporters: [{ type: 'html' }, { type: 'lcovonly' }, { type: 'text-summary' }]
         },
