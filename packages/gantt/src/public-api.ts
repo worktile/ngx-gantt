bed812b7af05e569de072d11eb25d4a75c1df131
zhangwen <1062680993@qq.com>
2021-07-21 15:48:35 +0800

    chore: bump docgeni 1.0.7 and enableIvy

--- packages/gantt/src/public-api.ts
+++ packages/gantt/src/public-api.ts
@@ -8,6 +8,8 @@ export * from './root.component';
 export * from './gantt.component';
 export * from './table/gantt-table.component';
 export * from './table/gantt-column.component';
+export * from './components/bar/bar.component';
+export * from './components/range/range.component';
 export * from './utils/date';
 export * from './class';
 export * from './views/view';
