025a9b28455ae40c9c7649a9396181a4d8d0e2c8
zhangwen <1062680993@qq.com>
2022-03-11 17:10:14 +0800

    fix: fix generateFSAndSFPath when line is curve

--- packages/gantt/src/components/links/lines/curve.ts
+++ packages/gantt/src/components/links/lines/curve.ts
@@ -4,8 +4,6 @@ import { GanttUpper, GANTT_UPPER_TOKEN } from '../../../gantt-upper';
 import { GanttLinkLine } from './line';
 
 export class GanttLinkLineCurve extends GanttLinkLine {
-    private bezierWeight = -0.5;
-
     constructor(@Inject(GANTT_UPPER_TOKEN) private ganttUpper: GanttUpper) {
         super();
     }
@@ -51,6 +49,7 @@ export class GanttLinkLineCurve extends GanttLinkLine {
         let y1 = source.after.y;
         let x4 = target.before.x;
         let y4 = target.before.y;
+        const bezierWeight = 0.5;
 
         if (type === GanttLinkType.sf) {
             x1 = target.after.x;
@@ -59,10 +58,10 @@ export class GanttLinkLineCurve extends GanttLinkLine {
             y4 = source.before.y;
         }
 
-        const dx = Math.abs(x4 - x1) * this.bezierWeight;
+        let dx = Math.abs(x4 - x1) * bezierWeight;
+        let x2 = x1 + dx;
+        let x3 = x4 - dx;
 
-        const x2 = x1 - dx;
-        const x3 = x4 + dx;
         const centerX = (x1 + x4) / 2;
         const centerY = (y1 + y4) / 2;
 
@@ -70,47 +69,28 @@ export class GanttLinkLineCurve extends GanttLinkLine {
         const controlY = this.ganttUpper.styles.lineHeight / 2;
 
         if (x1 >= x4) {
-            if (y4 > y1) {
-                if (Math.abs(y4 - y1) <= this.ganttUpper.styles.lineHeight) {
-                    return `M ${x1} ${y1}
-                    C ${x1 + controlX} ${y1} ${x1 + controlX} ${y1 + controlX} ${x1} ${y1 + controlY}
-                    L ${x4} ${y4 - controlY}
-                    C ${x4 - controlY} ${y4 - controlY}  ${x4 - controlX} ${y4} ${x4} ${y4}`;
-                } else {
-                    controlX = this.ganttUpper.styles.lineHeight;
-                    return `M ${x1} ${y1}
-                    C ${x1 + controlX} ${y1} ${x1 + controlX} ${y1 + controlX} ${centerX} ${centerY}
-                    C ${x4 - controlX} ${y4 - controlX} ${x4 - controlX} ${y4} ${x4} ${y4}
-                    `;
+            if (Math.abs(y4 - y1) <= this.ganttUpper.styles.lineHeight) {
+                return `M ${x1} ${y1}
+                    C ${x1 + controlX} ${y1} ${x1 + controlX} ${y4 > y1 ? y1 + controlX : y1 - controlX} ${x1} ${
+                    y4 > y1 ? y1 + controlY : y1 - controlY
                 }
+                    L ${x4} ${y4 > y1 ? y4 - controlY : y4 + controlY}
+                    C ${x4 - controlY} ${y4 > y1 ? y4 - controlY : y4 + controlY}  ${x4 - controlX} ${y4} ${x4} ${y4}
+                    `;
             } else {
-                if (Math.abs(y4 - y1) <= this.ganttUpper.styles.lineHeight) {
-                    return `M ${x1} ${y1}
-                    C ${x1 + controlX} ${y1} ${x1 + controlX} ${y1 - controlX} ${x1} ${y1 - controlY}
-                    L ${x4} ${y4 + controlY}
-                    C ${x4 - controlY} ${y4 + controlY} ${x4 - controlX} ${y4} ${x4} ${y4}
+                controlX = this.ganttUpper.styles.lineHeight;
+                return `M ${x1} ${y1}
+                    C ${x1 + controlX} ${y1} ${x1 + controlX} ${y4 > y1 ? y1 + controlX : y1 - controlX} ${centerX} ${centerY}
+                    C ${x4 - controlX} ${y4 > y1 ? y4 - controlX : y4 + controlX} ${x4 - controlX} ${y4} ${x4} ${y4}
                     `;
-                } else {
-                    controlX = this.ganttUpper.styles.lineHeight;
-                    return `M ${x1} ${y1}
-                    C ${x1 + controlX} ${y1} ${x1 + controlX} ${y1 - controlX} ${centerX} ${centerY}
-                    C ${x4 - controlX} ${y4 + controlX} ${x4 - controlX} ${y4} ${x4} ${y4}
-                    `;
-                }
             }
-        } else if (this.ganttUpper.linkOptions?.showArrow && x4 - x1 < 100) {
-            const radius = Math.abs(y4 - y1) / 4;
-            let lindWidth = x4 - x1 - radius;
-            lindWidth = Math.max(lindWidth, radius);
-
-            return `M ${x1} ${y1}
-                    L ${x1 + lindWidth} ${y1}
-                    A ${radius} ${radius} 0 1 ${y4 > y1 ? 1 : 0} ${x1 + lindWidth} ${y4 > y1 ? y1 + 2 * radius : y1 - 2 * radius}
-                    L ${x4 - lindWidth} ${y4 > y1 ? y1 + 2 * radius : y1 - 2 * radius}
-                    A ${radius} ${radius} 0 1 ${y4 > y1 ? 0 : 1} ${x4 - lindWidth} ${y4}
-                    L ${x4} ${y4}
-                    `;
+        } else if (this.ganttUpper.linkOptions?.showArrow && x4 - x1 < 200) {
+            dx = Math.max(Math.abs(y4 - y1) * bezierWeight, 60);
+            x2 = x1 + dx;
+            x3 = x4 - dx;
+            return `M ${x1} ${y1} C ${x2} ${y1} ${x3} ${y4} ${x4} ${y4}`;
         }
+
         return `M ${x1} ${y1} C ${x2} ${y1} ${x3} ${y4} ${x4} ${y4}`;
     }
 }
