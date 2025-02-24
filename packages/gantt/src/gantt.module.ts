90e7edfb2692446bae03ef9255b8ec82c548ea34
deku_nattsu <aymenbeta@gmail.com>
2025-02-24 11:49:57 +0100

    fix(gantt): removed unused import
    * fixes `Could not resolve ngx-gantt` error

--- packages/gantt/src/gantt.module.ts
+++ packages/gantt/src/gantt.module.ts
@@ -24,7 +24,6 @@ import { NgxGanttTableColumnComponent } from './table/gantt-column.component';
 import { NgxGanttTableComponent } from './table/gantt-table.component';
 import { GanttScrollbarComponent } from './components/scrollbar/scrollbar.component';
 import { i18nLocaleProvides } from './i18n';
-import { setDefaultTimeZone } from 'ngx-gantt';
 
 @NgModule({
     imports: [
