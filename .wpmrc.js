3f34aeb193051ebeda4ae10d16cbed56bab50e62
xinglu01 <15245457817@163.com>
2025-07-30 10:04:55 +0800

    fix(gantt): 纵向滚动条往下拖动会弹回到拉动的位置

--- .wpmrc.js
+++ .wpmrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    allowBranch: ['master', 'v15.*', 'v16.*', 'v17.*', 'release-auto-*'],
+    allowBranch: ['master', 'v15.*', 'v16.*', 'v17.*', 'v18.*', 'v19.*', 'release-auto-*'],
     bumpFiles: ['package.json', 'package-lock.json', 'packages/gantt/package.json'],
     skip: {
         confirm: true
